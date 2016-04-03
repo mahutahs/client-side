230e05e992a7677f26cdecf79b6e3b5f5f00566b
Mahuta <mahutahs@yahoo.co.nz>
2016-04-04 11:39:31 +1200

    Updates

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var example = require('./views/example.hbs')
 
 xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
   if (err) console.log(err) // do something
-  JSON.parse(index.html)
-  console.log()
+
+  console.log("got this far")
   document.body.innerHTML = example({ name: "Space" });
 })
