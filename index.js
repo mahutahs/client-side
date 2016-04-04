87096a98b3523bad5894f3c38ab27c1867981e90
Mahuta <mahutahs@yahoo.co.nz>
2016-04-04 16:14:00 +1200

    Updates

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ var example = require('./views/example.hbs')
 
 
 xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
+console.log("hello")
   if (err) {throw err} // do something
     console.log('all data: ',data)
-    console.log('just body: ', data.body)
-var receivedData = JSON.parse(data.body)
-//console.log(data.body)
+    console.log('just body: ', data.body[0])
 
-  console.log(receivedData)
 
 //  document.body.innerHTML = example(receivedData);
 })
