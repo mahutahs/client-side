65bcacc2c47103e24a848251adadc71ff85ef98d
Mahuta <mahutahs@yahoo.co.nz>
2016-04-04 16:17:24 +1200

    Updates

--- bundle.js
+++ bundle.js
@@ -7,7 +7,10 @@ xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
 console.log("hello")
   if (err) {throw err} // do something
     console.log('all data: ',data)
-    console.log('just body: ', data.body[0])
+    console.log('just body: ', data.body)
+    var data2 = JSON.parse(data.body)
+    console.log('data2',data2)
+    console.log('data2 0',data2[0])
 
 
 //  document.body.innerHTML = example(receivedData);
