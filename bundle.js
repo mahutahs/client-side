6ad30ce764f4ffe1a28e5ff62fa65bec8b246bd5
Mahuta <mahutahs@yahoo.co.nz>
2016-04-04 12:21:49 +1200

    Updates

--- bundle.js
+++ bundle.js
@@ -5,9 +5,10 @@ var example = require('./views/example.hbs')
 
 xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
   if (err) console.log(err) // do something
+var receivedData = JSON.parse(data.body)
 
-  console.log("got this far")
-  document.body.innerHTML = example({name:'space'});
+  console.log(receivedData)
+  document.body.innerHTML = example(receivedData.name);
 })
 
 },{"./views/example.hbs":30,"xhr":22}],2:[function(require,module,exports){
--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var example = require('./views/example.hbs')
 
 xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
   if (err) console.log(err) // do something
+var receivedData = JSON.parse(data.body)
 
-  console.log("got this far")
-  document.body.innerHTML = example({name:'space'});
+  console.log(receivedData)
+  document.body.innerHTML = example(receivedData.name);
 })
